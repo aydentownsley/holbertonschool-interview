10cc127d6d880c54cb5f384caa6bb3428f1c0c38
aydentownsley <aydentownsley@gmail.com>
2021-07-21 06:04:11 +0000

    docs and pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""
+LockBox Module
+-------
+This module houses the defs for
+recursing through the boxes
+"""
 
 
 def box_recurse(boxes, unlocked, box):
+    """
+    boxes: list of lockboxes
+    unlocked: list of keys
+    box: current box
+    """
     if len(unlocked) == len(boxes):
         return True
     for key in boxes[box]:
@@ -11,9 +22,13 @@ def box_recurse(boxes, unlocked, box):
             if box_recurse(boxes, unlocked, key) is True:
                 return True
     return False
-        
+
 
 def canUnlockAll(boxes):
+    """
+    creates empty list and starts
+    recursion at 0
+    """
     unlocked = [0]
     box = 0
     return(box_recurse(boxes, unlocked, box))
