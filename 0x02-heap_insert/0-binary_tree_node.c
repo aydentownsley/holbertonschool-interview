034b96202f0496e59338c724deb917565fbf80ce
aydentownsley <aydentownsley@gmail.com>
2021-08-04 21:11:03 +0000

    set l and r to NULL

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,19 +15,16 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
-	{
-		free(node);
 		return (NULL);
-	}
+
+	node->n = value;
 
 	if (parent == NULL)
-	{
-		parent = node;
-		node->n = value;
-		return (node);
-	}
+		node->parent = NULL;
+	else
+		node->parent = parent;
 
-	node->parent = parent;
-	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
