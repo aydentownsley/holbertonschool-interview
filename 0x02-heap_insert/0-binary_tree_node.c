8a8d31e2795b8f479004af0f4ab35b8936d807d4
aydentownsley <aydentownsley@gmail.com>
2021-08-04 20:57:39 +0000

    adding if parent in NULL

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,22 +1,32 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  *
  * @parent: the parent node
  * @value: the value of the node
  *
- * Returns a pointer to the newly created node.
+ * Return: a pointer to the newly created node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
 	{
-		free (node);
-		return NULL;
+		free(node);
+		return (NULL);
 	}
+
+	if (parent == NULL)
+	{
+		parent = node;
+		node->n = value;
+		return (node);
+	}
+
 	node->parent = parent;
 	node->n = value;
 	return (node);
