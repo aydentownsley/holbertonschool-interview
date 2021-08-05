4d4babb8b840f65c699fbc839f9baf00521df42f
aydentownsley <aydentownsley@gmail.com>
2021-08-05 04:09:19 +0000

    adding basic add

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -7,9 +7,20 @@
  * @root: root of tree
  * @value: the value of the node
  *
- * Returns a pointer to inserted node
+ * Return: a pointer to inserted node
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
+	heap_t *node = NULL;
 
+	node = binary_tree_node(*root, value);
+
+	if (node == NULL)
+		return (NULL);
+
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = *root;
+
+	return (node);
 }
