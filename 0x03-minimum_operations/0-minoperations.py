f4ad62bc68be14b8c1ced0ea73956ba8e016c783
Ayden Townsley <2499@holbertonschool.com>
2021-08-11 00:52:40 -0500

    adding project

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """
+    Args: n [integer]
+    ----
+    Finds minumum operations required
+    to make n Hs given only Copy All
+    and Paste.
+    Returns: min ops [int]
+    Prime or Odd not div by 3: n - 1
+    Even: unknown
+    Modulo by 3: n/3 + 3
+    """
+    curr = 1
+    pasteVal = 1
+    ops = 1
+    if n % 2 == 0:
+        num = 1
+        while (num < n / 2):
+            if (n % num == 0):
+                div1 = num
+                div2 = n / num
+            num += 1
+        print("num1: {}, num2: {}".format(int(div1), int(div2)))
+        while curr != n:
+            if curr + pasteVal > n:
+                break
+            curr = curr + pasteVal
+            ops += 1
+            if curr * 2 > n:
+                break
+            pasteVal = curr * 2
+            ops += 2
+        return ops
+    elif (n % 3 == 0):
+        if n == 3:
+            return 3
+        return int(n / 3 + 3)
+    else:
+        return n - 1
+
+
+def isPrime(n):
+    """
+    Args: n [integer]
+    ----
+    Tests if number is prime
+    Returns: True is prime
+             False if not
+    """
+    if n < 2:
+         return False;
+    if n % 2 == 0:
+         return n == 2
+    k = 3
+    while k*k <= n:
+         if n % k == 0:
+             return False
+         k += 2
+    return True
---
+++ 0x03-minimum_operations/README.md
@@ -0,0 +1,4 @@
+## 0x03. Minimum Operations
+
+Given a letter H and the operations **Copy all** and **Paste**,\
+what is the minimum number of operations to reach a given number of characters?
