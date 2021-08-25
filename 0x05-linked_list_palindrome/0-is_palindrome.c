beccf772f7459ef81f0bebea88201687d85def95
aydentownsley <aydentownsley@gmail.com>
2021-08-25 22:25:02 +0000

    adding interview q

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ *
+ * @head: head of list
+ *
+ * Return: 0 if not a palindrome
+ * OR 1 if is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *cursor = *head, *tailcheck = *head;
+	int i = 0, idx = 0, itr = 0;
+
+	while (cursor->next)
+	{
+		cursor = cursor->next;
+		i++;
+	}
+
+	cursor = *head;
+	for (idx = 0; idx < i; idx++)
+	{
+		tailcheck = *head;
+		itr = 0;
+		while (itr < (i - idx))
+		{
+			tailcheck = tailcheck->next;
+			itr++;
+		}
+		if (tailcheck->n == cursor->n)
+			cursor = cursor->next;
+		else
+			return (0);
+	}
+
+	return (1);
+}
