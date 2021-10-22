dd9db9e7f6a5fafae779711892d2b25b22e06541
aydentownsley <aydentownsley@gmail.com>
2021-10-22 14:49:22 +0000

    testing new method after some research

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,91 +3,81 @@
 import sys
 
 
-def board_size():
-    """gets board_board_size of board from user
-    """
-    if (len(sys.argv) != 2):
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    if (n < 4):
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
+def solve(n):
+    """ handles all solutions """
+    board = create_board(n)
+    add_queens(board)
 
 
-def row_full(board, row):
-    """checks if row is full"""
-    for full in board[row]:
-        if full:
-            return True
-    return False
+def create_board(n):
+    """ creates and empty chess board of size n """
+    board = []
+    row = []
+    if isinstance(n, int):
+        row = ["[ ]" for i in range(n)]
+        board = [row.copy() for i in range(n)]
+        return board
 
 
-def col_full(board, col):
-    """checks if col is full """
-    board_size = len(board[0])
-    for row in range(board_size):
-        full = board[row][col]
-        if full:
-            return True
-    return False
+def add_queens(board, idx=0):
+    """ adds queens to the board """
+    next = idx + 1
+    row = board[idx]
+    for col in range(len(row)):
+        row[col] = "Q"
+        if is_valid(board):
+            if next < len(board):
+                add_queens(board, next)
+            elif next == len(board):
+                print_board(board)
+        row[col] = "[ ]"
 
 
-def diag_full(board, row, col):
-    """checks if diagonal is full
-    """
-    for x, y in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[x][y]:
-            return True
+def is_valid(board):
+    """ returns true if valid organization for queens """
+    queens = []
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == "Q":
+                queens.append((row, col))
 
-    for x, y in zip(range(row, len(board[0]), 1),
-                    range(col, -1, -1)):
-        if board[x][y]:
-            return True
+    for queen in queens:
+        for other in queens:
+            x1, y1 = queen[0], queen[1]
+            x2, y2 = other[0], other[1]
+            if x1 == x2 or y1 == y2:
+                continue
+            elif x1 == x2 and y1 == y2:
+                return False
+            elif (abs((x1 - x2) / (y1 - y2)) == 1):
+                return False
+    return True
 
-    return False
 
-
-def print_board(board, s):
-    """ prints board """
-    queens = [[i, j] for i in range(s) for j in range(s) if board[i][j] == 1]
+def print_board(board):
+    """ prints the board pos of valid organizations """
+    queens = []
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == "Q":
+                queens.append([row, col])
     print(queens)
 
 
-def n_queens():
-    """ create chess board """
-    size = board_size()
-    board = [[False for j in range(size)] for i in range(size)]
-    solve_board(0, 0, board)
-
-
-def solve_board(pos, queens, board):
-    """ recurrrrrrsion """
-    board_size = len(board[0])
-    s = [(j, i) for j in range(board_size) for i in range(board_size)]
-    if queens == board_size:
-        print_board(board, board_size)
-        return True
-    elif pos >= len(s):
-        return False
-    i, j = s[pos]
-    if not row_full(board, i) and not col_full(board, j) \
-       and not diag_full(board, i, j):
-        queens += 1
-    b_true = [row[:] for row in board]
-    b_false = [row[:] for row in board]
-    b_true[i][j] = True
-    b_false[i][j] = False
-    if solve_board(pos + 1, queens, b_true) or \
-       solve_board(pos + 1, queens, b_false):
-        return True
+def start():
+    if (len(sys.argv) < 2):
+        print("Usage: nqueens N")
+        sys.exit(1)
+    n = sys.argv[1]
+    if not n.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(n)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solve(n)
 
 
 if __name__ == "__main__":
-    n_queens()
+    start()
