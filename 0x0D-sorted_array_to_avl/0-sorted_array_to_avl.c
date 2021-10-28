538de6c3e1b2a68ef48b4c569c8fd35a153cc46e
aydentownsley <aydentownsley@gmail.com>
2021-10-28 17:37:41 +0000

    I was rewritign my root node over and over, needed to pass it

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -4,32 +4,56 @@
 #include "binary_trees.h"
 
 /**
+ * create_node - creates a new node
+ *
+ * @data: data to be stored in the new node
+ * @root: root node of avl
+ * Return: pointer to the new node
+ */
+avl_t *create_node(avl_t *root, int data)
+{
+	avl_t *new_node = malloc(sizeof(avl_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = data;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (root == NULL)
+	{
+		new_node->parent = NULL;
+		root = new_node;
+	}
+	else
+		new_node->parent = root;
+
+	return (new_node);
+}
+
+/**
  * smart_insert - inserts node from sorted array in order
  *
  * @array: array of to be inserted
  * @begin: start of sorted array [section]
  * @end: end of sorted array [section]
+ * @root: root node of avl
  *
  * Return: root of tree
  */
-avl_t *smart_insert(int *array, int begin, int end)
+avl_t *smart_insert(int *array, int begin, int end, avl_t *root)
 {
 	int mid;
-	avl_t *root;
-	avl_t *new_node = (avl_t *)malloc(sizeof(avl_t));
-
-	if (new_node == NULL)
-		return (NULL);
 
 	if (begin > end)
 		return (NULL);
 
 	mid = (begin + end) / 2;
-	root = new_node;
-	root->n = array[mid];
+	root = create_node(root, array[mid]);
 
-	root->left = smart_insert(array, begin, mid - 1);
-	root->right = smart_insert(array, mid + 1, end);
+	root->left = smart_insert(array, begin, mid - 1, root);
+	root->right = smart_insert(array, mid + 1, end, root);
 
 	return (root);
 }
@@ -43,10 +67,8 @@ avl_t *smart_insert(int *array, int begin, int end)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	size_t begin = 0, end = size - 1;
-	avl_t *root;
+	if (array == NULL || size == 0)
+		return (NULL);
 
-	root = smart_insert(array, begin, end);
-
-	return (root);
+	return (smart_insert(array, 0, size - 1, NULL));
 }
