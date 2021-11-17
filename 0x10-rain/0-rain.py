a48500f1faf065c2ef02c58753127a5a365bf19f
aydentownsley <aydentownsley@gmail.com>
2021-11-17 21:08:40 +0000

    fixing indexing of recursion and finding max values

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,45 +10,55 @@ def rain(walls):
         walls (List): list of height from 0 to n
     """
     # instantiate int vars and list vars
-    area = neg_area = 0
+    area = neg_area = mx_0 = mx_1 = 0
     left_walls = right_walls = []
 
     # checks for empty list
-    if len(walls) == 0:
+    if len(walls) <= 2:
         return 0
 
     # finds top 2 max values (and null check)
-    copy_walls = walls[:]
-    mx_0 = max(copy_walls)
-    copy_walls.remove(mx_0)
-    if len(copy_walls) == 0:
+    mx_0 = max(walls)
+    for i in range(len(walls)):
+        if i != walls.index(mx_0) and mx_1 < walls[i]:
+            mx_1 = walls[i]
+            index_mx_1 = i
+
+    if mx_0 == 0 or mx_1 == 0:
         return 0
-    mx_1 = max(copy_walls)
+
+    #  print("mx0: {}, mx1: {}".format(mx_0, mx_1))
 
     # determines which way to iterated in for loops
-    if walls.index(mx_1) > walls.index(mx_0):
+    if index_mx_1 > walls.index(mx_0):
         l_idx = walls.index(mx_0)
-        r_idx = walls.index(mx_1)
+        r_idx = index_mx_1
     else:
-        l_idx = walls.index(mx_1)
+        l_idx = index_mx_1
         r_idx = walls.index(mx_0)
 
     # creates slices of lists for recursion
     left_walls = walls[0:l_idx + 1]
     right_walls = walls[r_idx:len(walls) - 1]
+    print(left_walls)
+    print(right_walls)
 
     # calculates area between walls and subtracts height of
     # walls in between
-    if mx_0 > mx_1:
+    if mx_0 >= mx_1:
         for x in range(l_idx + 1, r_idx):
             neg_area -= walls[x]
-            area = mx_1 * (r_idx - l_idx - 1)
-            area -= neg_area
+            print("na: {}".format(neg_area))
+        area = mx_1 * (r_idx - l_idx - 1)
+        print("area_0: {}".format(area))
+        area += neg_area
+        print("area_1: {}".format(area))
     else:
         for x in range(l_idx + 1, r_idx):
             neg_area -= walls[x]
-            area = mx_0 * (r_idx - l_idx - 1)
-            area -= neg_area
+            print("na: {}".format(neg_area))
+        area = mx_0 * (r_idx - l_idx - 1)
+        area += neg_area
 
     # adds recursed area to area
     area += rain(left_walls)
