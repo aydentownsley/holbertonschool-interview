37ef26d1466d01aef9440e1c0da13201ec88509e
aydentownsley <aydentownsley@gmail.com>
2021-12-01 22:23:27 +0000

    fixing print

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -45,6 +45,7 @@ unsigned int high, size_t size, int value)
 {
 	unsigned int mid = 0;
 
+	sleep(1);
 
 	high = size - 1;
 	mid = (high + low) / 2;
@@ -53,6 +54,7 @@ unsigned int high, size_t size, int value)
 	{
 		if (array[mid] == value)
 		{
+			printf("==\n");
 			if (array[mid - 1] == value && mid != 1)
 			{
 				print_array(&array[low], high - mid);
@@ -63,12 +65,14 @@ unsigned int high, size_t size, int value)
 		}
 		if (array[mid] > value)
 		{
-			print_array(&array[mid - 1], high - mid);
-			return (adv_bin_help(array, mid - 1, mid - low, size, value));
+			printf(">\n");
+			print_array(&array[low], high - mid);
+			return (adv_bin_help(array, low, mid, mid + 1, value));
 		}
 		if (array[mid] < value)
 		{
-			print_array(&array[mid + 1], high - mid);
+			printf("<\n");
+			print_array(&array[mid + 1], high - (mid));
 			return (adv_bin_help(array, mid + 1, high - mid, size, value));
 		}
 	}
