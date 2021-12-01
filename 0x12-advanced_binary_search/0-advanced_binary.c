22d4d739164622ec68948c53d2df6e2c11d3d618
aydentownsley <aydentownsley@gmail.com>
2021-12-01 23:00:41 +0000

    getting rid of useless variable

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -33,32 +33,27 @@ void print_array(int *array, size_t size)
  *
  * @array: array to search
  * @low: low value
- * @high: high value
  * @size: size of array
  * @value: value to look for
  *
  * Return: VALUE if found
  * -1 if not
  */
-int adv_bin_help(int *array, unsigned int low,
-unsigned int high, size_t size, int value)
+int adv_bin_help(int *array, unsigned int low, size_t size, int value)
 {
 	unsigned int mid = 0;
 
+	mid = ((size - 1) + low) / 2;
 
-
-	high = size - 1;
-	mid = (high + low) / 2;
-
-	if (low <= high)
+	if (low <= (size - 1))
 	{
 		if (array[mid] == value)
 		{
 			/*printf("==\n");*/
 			if (array[mid - 1] == value && mid != 1)
 			{
-				print_array(&array[low], high - mid);
-				adv_bin_help(array, low, high, mid + 1, value);
+				print_array(&array[low], (size - 1) - mid);
+				adv_bin_help(array, low, mid + 1, value);
 				return (mid - 1);
 			}
 			return (mid);
@@ -66,14 +61,14 @@ unsigned int high, size_t size, int value)
 		if (array[mid] > value)
 		{
 			/*printf(">\n");*/
-			print_array(&array[low], high - mid);
-			return (adv_bin_help(array, low, mid, mid + 1, value));
+			print_array(&array[low], (size - 1) - mid + 1);
+			return (adv_bin_help(array, low, mid + 1, value));
 		}
 		if (array[mid] < value)
 		{
 			/*printf("<\n");*/
-			print_array(&array[mid + 1], high - (mid));
-			return (adv_bin_help(array, mid + 1, high - mid, size, value));
+			print_array(&array[mid + 1], (size - 1) - (mid));
+			return (adv_bin_help(array, mid + 1, size - 1, value));
 		}
 	}
 
@@ -92,12 +87,11 @@ unsigned int high, size_t size, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	unsigned int low = 0, high = 0;
+	unsigned int low = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	high = size - 1;
 	print_array(array, size);
-	return (adv_bin_help(array, low, high, size, value));
+	return (adv_bin_help(array, low, size, value));
 }
