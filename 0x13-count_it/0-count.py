d94a1192a37f978f06656f25f08eb2c2da64b764
aydentownsley <aydentownsley@gmail.com>
2021-12-08 20:51:25 +0000

    pep8 docs

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,6 +7,13 @@ import sys
 
 
 def count_words(subreddit, word_list, count=0):
+    """gets count of words in hot list of subreddit
+
+    Args:
+        subreddit ([type]): [description]
+        word_list ([type]): [description]
+        count (int, optional): [description]. Defaults to 0.
+    """
     number = 0
     headers = {'User-Agent': 'parse_hot'}
     url = "http://www.reddit.com/r/{}/hot.json".format(sys.argv[1])
