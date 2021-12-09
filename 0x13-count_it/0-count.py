f5d7e206e6eb0bf50857f0379e5ef9d47b8d8e72
aydentownsley <aydentownsley@gmail.com>
2021-12-09 01:46:18 +0000

    adding 404 check

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -14,16 +14,27 @@ def count_words(subreddit, word_list, count=0):
         word_list ([type]): [description]
         count (int, optional): [description]. Defaults to 0.
     """
+    new_list = []
+    for i in word_list:
+        i.lower()
+    for i in word_list:
+        if i not in new_list:
+            new_list.append(i)
     number = 0
     headers = {'User-Agent': 'parse_hot'}
-    url = "http://www.reddit.com/r/{}/hot.json".format(sys.argv[1])
+    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers)
+    if response.status_code == 404:
+        exit()
     data = response.json()
     for value in response.json()['data']['children']:
         for res in value['data'].items():
             if res[0] == 'title':
-                if word_list[count] in res[1].lower():
-                    number += 1
-    if count + 1 < len(word_list):
-        count_words(subreddit, word_list, count + 1)
-    print("{}: {}".format(word_list[count], number))
+                if new_list[count] in res[1].lower():
+                    tile_arr = res[1].lower().split()
+                    for i in tile_arr:
+                        if i == new_list[count]:
+                            number += 1
+    if count + 1 < len(new_list):
+        count_words(subreddit, new_list, count + 1)
+    print("{}: {}".format(new_list[count], number))
