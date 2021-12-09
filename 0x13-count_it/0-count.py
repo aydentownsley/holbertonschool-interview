3c8e5448d4ca494b3c085d234a28001300eda93e
aydentownsley <aydentownsley@gmail.com>
2021-12-09 03:28:21 +0000

    wasnt asking api of reddit, needed to rework

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,10 +3,9 @@
     a given subreddit and count given keywords
 """
 import requests
-import sys
 
 
-def count_words(subreddit, word_list, count=0):
+def count_words(subreddit, word_list, counts={}, after=None):
     """gets count of words in hot list of subreddit
 
     Args:
@@ -14,27 +13,43 @@ def count_words(subreddit, word_list, count=0):
         word_list ([type]): [description]
         count (int, optional): [description]. Defaults to 0.
     """
+    if after:
+        url = "https://api.reddit.com/r/{}/hot?after={}".format(
+            subreddit, after)
+    else:
+        url = "http://api.reddit.com/r/{}/hot".format(subreddit)
+
     new_list = []
     for i in word_list:
         i.lower()
     for i in word_list:
         if i not in new_list:
             new_list.append(i)
-    number = 0
+
     headers = {'User-Agent': 'parse_hot'}
-    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers)
-    if response.status_code == 404:
-        exit()
-    data = response.json()
-    for value in response.json()['data']['children']:
-        for res in value['data'].items():
-            if res[0] == 'title' or res[0] == 'selftext':
-                if new_list[count] in res[1].lower():
-                    tile_arr = res[1].lower().split()
-                    for i in tile_arr:
-                        if i == new_list[count]:
-                            number += 1
-    if count + 1 < len(new_list):
-        count_words(subreddit, new_list, count + 1)
-    print("{}: {}".format(new_list[count], number))
+
+    try:
+        data = response.json()
+    except:
+        return None
+
+    if 'data' not in data:
+        return None
+
+    for value in data['data']['children']:
+        for res in value['data']['title'].split():
+            for i in new_list:
+                if i.lower() == res.lower():
+                    if i.lower() in counts.keys():
+                        counts[i.lower()] += 1
+                    else:
+                        counts[i.lower()] = 1
+
+    after = data['data']['after']
+
+    if after:
+        return (count_words(subreddit, word_list, counts, after))
+
+    for key in sorted(counts, key=counts.get, reverse=True):
+        print("{}: {}".format(key, counts[key]))
