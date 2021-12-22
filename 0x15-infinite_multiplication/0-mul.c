a402def811f81491e6c18cfdab0dd1c94ba85578
aydentownsley <aydentownsley@gmail.com>
2021-12-22 15:43:39 +0000

    adding mul and readme

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void pr(char to_print[]);
+
+/**
+ * main - handles input and mult
+ *
+ * @argc: lenght of input
+ * @argv: array of input
+ *
+ * Return: 98 if error
+ * final mult string if successful
+ */
+int main(int argc, char *argv[])
+{
+	int a[1000], b[1000];
+	int ans[2001] = {0};
+	int i, j, tmp, l1, l2;
+
+	if (argc != 3)
+	{
+		pr("Error\n");
+		exit(98);
+	}
+
+	if (argv == NULL)
+	{
+		pr("Error\n");
+		exit(98);
+	}
+
+	l1 = strlen(argv[1]);
+	l2 = strlen(argv[2]);
+
+	for (i = l1 - 1, j = 0; i >= 0; i--, j++)
+	{
+		a[j] = argv[1][i] - '0';
+	}
+	for (i = l2 - 1, j = 0; i >= 0; i--, j++)
+	{
+		b[j] = argv[2][i] - '0';
+	}
+	for (i = 0; i < l2; i++)
+	{
+		for (j = 0; j < l1 ; j++)
+		{
+			ans[i + j] += b[i] * a[j];
+		}
+	}
+	for (i = 0; i < l1 + l2; i++)
+	{
+		tmp = ans[i] / 10;
+		ans[i] = ans[i] % 10;
+		ans[i + 1] = ans[i + 1] + tmp;
+	}
+	for (i = l1 + l2; i >= 0; i--)
+	{
+		if (ans[i] > 0)
+			break;
+	}
+
+	for (; i >= 0; i--)
+	{
+		_putchar(ans[i] + '0');
+	}
+	_putchar(10);
+	return (0);
+}
+
+/**
+ * pr - prints a string
+ *
+ * @to_print: char array to print
+ *
+ * Return: void
+ */
+void pr(char to_print[])
+{
+	int i = 0;
+
+	while (to_print[i] != '\0')
+	{
+		_putchar(to_print[i]);
+		++i;
+	}
+}
