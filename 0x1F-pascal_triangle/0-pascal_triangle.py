3531268971d9b766813476cee7cb99a784c5ecef
Ayden Townsley <aydentownsley@gmail.com>
2022-04-11 09:54:02 -0500

    adding pascals triangle

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+	Pascal's Triangle Module
+"""
+
+
+def pascals_triangel(n):
+	"""
+	return a 2D array representing
+	a pascal triangle of n size
+	"""
+
+	# define triangle array and "tip" of triangle
+	triangle =[]
+	row = [1]
+
+	for i in range(n):
+		triangle.append(row)
+		next_row = []
+		next_row.append(row[0])
+		for i in range(len(row) - 1):
+			next_row.append(row[i] + row[i+1])
+		next_row.append(row[0])
+		row = next_row
+	return triangle
---
+++ 0x1F-pascal_triangle/README.md
@@ -0,0 +1,6 @@
+# Pascals Triangle
+
+# Create a pascal triangle of n size.
+
+# To do this we need to create a 2D array that takes into account elements
+# of the previous row in order to build the current row.
