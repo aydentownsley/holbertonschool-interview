b4c5c3bc4c416408f5981df5631b48982340ac4f
aydentownsley <aydentownsley@gmail.com>
2022-04-16 16:31:56 +0000

    pep8

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-	Pascal's Triangle Module
+    Pascal's Triangle Module
 """
 
 
 def pascal_triangle(n):
-	"""
-		n: size of triangle to create
-	"""
+    """
+        n: size of triangle to create
+    """
 
-	# define triangle array and "tip" of triangle
-	triangle =[]
-	row = [1]
+    # define triangle array and "tip" of triangle
+    triangle = []
+    row = [1]
 
-	for i in range(n):
-		triangle.append(row)
-		next_row = []
-		next_row.append(row[0])
-		for i in range(len(row) - 1):
-			next_row.append(row[i] + row[i+1])
-		next_row.append(row[0])
-		row = next_row
-	return triangle
+    for i in range(n):
+        triangle.append(row)
+        next_row = []
+        next_row.append(row[0])
+        for i in range(len(row) - 1):
+            next_row.append(row[i] + row[i+1])
+        next_row.append(row[0])
+        row = next_row
+    return triangle
